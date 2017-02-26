9f38db39c70c530611269d4f9e793debee123fbe
Charandeep Rai <charandeep.rai@gmail.com>
2017-02-26 17:27:51 +0000

    Adding Teardown Methods for the GUITest class

--- application/tests/GUITest.java
+++ application/tests/GUITest.java
@@ -1,13 +1,20 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeoutException;
+
+import org.junit.After;
 import org.junit.Test;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 
 public class GUITest extends ApplicationTest{
@@ -26,9 +33,14 @@ public class GUITest extends ApplicationTest{
 //		return lookup(query).query();
 //	}
 	
-	@Override
-	public void stop(){
-		
+	@After
+	public void stop() throws TimeoutException{
+	FxToolkit.hideStage();
+	release(new KeyCode[] {
+	});
+	release(new MouseButton[]{
+	});
+	
 	}
 	
 
