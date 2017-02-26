ce78b4b2c5316f01b97bffd0472e6581c42647dd
Charandeep Rai <charandeep.rai@gmail.com>
2017-02-26 17:22:49 +0000

    Adding TestFX tests for the GUI

--- application/tests/GUITest.java
+++ application/tests/GUITest.java
@@ -1,14 +1,35 @@
 package tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
 
-public class GUITest {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class GUITest extends ApplicationTest{
+
+	
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent loadNode = FXMLLoader.load(getClass().getResource("/view/GherkinView.fxml"));
+		stage.setScene(new Scene(loadNode));
+		stage.show();
+		stage.toFront();
 	}
+	
+//	public <T extends Node> Object find (final String query){
+//		return lookup(query).query();
+//	}
+	
+	@Override
+	public void stop(){
+		
+	}
+	
 
 }
