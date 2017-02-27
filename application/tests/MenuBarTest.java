6965f3e83f547d7f8090e5fc4228edb9f6c5d8ac
Charandeep Rai <charandeep.rai@gmail.com>
2017-02-27 19:24:39 +0000

    Adding class to test the MenuBar

--- application/tests/MenuBarTest.java
+++ application/tests/MenuBarTest.java
@@ -1,5 +1,18 @@
 package tests;
 
-public class MenuBarTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.testfx.matcher.base.NodeMatchers;
+import org.testfx.api.FxRobotException;
+import static org.testfx.api.FxAssert.verifyThat;
+import org.testfx.util.WaitForAsyncUtils;
+
+public class MenuBarTest extends AbstractTest {
+
+	final String SAVE_CONTENTS = "#saveConents";
+	final String LOAD_CONTENTS = "#loadFile";
+	final String EXIT_APPLICATION = "#exit";
+
+
 
 }
