4aee79d27da1207942a1f7e7f38641ad63967290
Charandeep Rai <charandeep.rai@gmail.com>
2017-02-28 01:26:58 +0000

    Added further tests to the SideBarTest Class

--- application/tests/SideBarTest.java
+++ application/tests/SideBarTest.java
@@ -20,7 +20,6 @@ public class SideBarTest extends AbstractTest {
 	
 	@Test
 	public void clickOnLoadButton(){
-		
 		verifyThat(LOAD_BUTTON, NodeMatchers.isNotNull());
 		verifyThat(LOAD_BUTTON, NodeMatchers.hasText("Load Test Case"));
 		clickOn("#loadButton");
@@ -28,23 +27,29 @@ public class SideBarTest extends AbstractTest {
 	
 	@Test
 	public void clickOnSaveButton(){
+		verifyThat(SAVE_BUTTON, NodeMatchers.isNotNull());
+		verifyThat(SAVE_BUTTON, NodeMatchers.hasText("Save Test Case"));
 		clickOn("#saveButton");
 	}
 	
 	@Test
 	public void clickOnValidateButton(){
+		verifyThat(VALIDATE_BUTTON, NodeMatchers.isNotNull());
+		verifyThat(VALIDATE_BUTTON, NodeMatchers.hasText("Validate Script"));
 		clickOn("#validateButton");
 	}
 	
 	@Test
 	public void clickOnStepDefsButton(){
+		verifyThat(STEPDEFS_BUTTON, NodeMatchers.isNotNull());
+		verifyThat(STEPDEFS_BUTTON, NodeMatchers.hasText("Create Step Defs"));
 		clickOn("#createStepDefsButton");
 	}
 	
 	@Test
 	public void clickOnSeleniumButton(){
-		
-		
+		verifyThat(SELENIUM_BUTTON, NodeMatchers.isNotNull());
+		verifyThat(SELENIUM_BUTTON, NodeMatchers.hasText("Selenium View"));
 		clickOn("#seleniumViewButton");
 
 	}
@@ -53,6 +58,13 @@ public class SideBarTest extends AbstractTest {
 	public void checkLabelExists(){	
 		verifyThat(LABEL, NodeMatchers.isNotNull());
 		verifyThat(LABEL, NodeMatchers.hasText("Created by Charandeep Rai"));
+	}
+	
+	@Test
+	public void clickOnCloseButton(){
+		verifyThat(CLOSE_BUTTON, NodeMatchers.isNotNull());
+		verifyThat(CLOSE_BUTTON, NodeMatchers.hasText("Exit Application"));
+		clickOn("#closeButton");
 	}
 	
 }
