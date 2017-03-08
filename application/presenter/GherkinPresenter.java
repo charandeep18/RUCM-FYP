9addd741bb4c8374e298dc215c3e885b51560ef8
Charandeep Rai <charandeep.rai@gmail.com>
2017-03-08 11:03:16 +0000

    Adding Initializable to GherkinPresenter

--- application/presenter/GherkinPresenter.java
+++ application/presenter/GherkinPresenter.java
@@ -3,8 +3,10 @@ package presenter;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Collection;
@@ -14,6 +16,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -40,7 +43,7 @@ import model.openFile;
 import model.saveFile;
 //import model.checkSyntax;
 
-public class GherkinPresenter {
+public class GherkinPresenter implements Initializable {
 	    
 // FXML Containers for GherkinView
 	
@@ -104,12 +107,16 @@ public class GherkinPresenter {
 
 //FXML Items for GUI-Right 		
 
-	    @FXML
+	   // @FXML
 	    public CodeArea readTextArea;
 	    
-	    @FXML
+	    //@FXML
 	    public static TextArea validationArea;
 	   
+
+	    
+
+	    
 	    @FXML
 	    public void OpenFile(ActionEvent event) {
 	    	// Setting the filter to show only text files
@@ -175,6 +182,13 @@ public class GherkinPresenter {
 			}
 
 	    }
+
+
+		@Override
+		public void initialize(URL location, ResourceBundle resources) {
+			// TODO Auto-generated method stub
+			
+		}
 	    
 	}
 	
