5e58bf049225e7f4630cf36fdcf0bc41b83b00b6
Charandeep Rai <charandeep.rai@gmail.com>
2017-03-09 00:52:30 +0000

    Attempting to add the Code Area

--- application/presenter/TestPresenter.java
+++ application/presenter/TestPresenter.java
@@ -1,5 +1,31 @@
 package presenter;
 
-public class TestPresenter {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+
+public class TestPresenter implements Initializable
+{
+
+
+    @FXML
+    private SplitPane splitPane;
+
+    @FXML
+    private AnchorPane left;
+
+    @FXML
+    private AnchorPane right;
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
