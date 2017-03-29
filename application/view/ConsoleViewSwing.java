026c6650c63447900748068e2d8755a6435ee4dc
Charandeep Rai <charandeep.rai@gmail.com>
2017-03-29 02:07:41 +0100

    Adding the JavaFX framework for the Swing container

--- application/view/ConsoleViewSwing.java
+++ application/view/ConsoleViewSwing.java
@@ -1,5 +1,29 @@
 package view;
 
-public class ConsoleViewSwing {
+import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class ConsoleViewSwing extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		final SwingNode swingNode = new SwingNode();
+		swingContent(swingNode);
+		
+		StackPane pane = new StackPane();
+		pane.getChildren().add(swingNode);
+		
+		primaryStage.setTitle("Console View");
+		primaryStage.setScene(new Scene(pane, 300, 200));
+		primaryStage.show();
+		
+		
+	}
+	
+	public void swingContent(final SwingNode swingNode) {
+		
+	}
 }
