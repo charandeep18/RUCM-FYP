b4e27bd2221142b22bfda0f3fde03ec346d43ca2
Charandeep Rai <charandeep.rai@gmail.com>
2017-04-04 01:53:15 +0100

    Adding both console and printstream variables

--- application/model/Console.java
+++ application/model/Console.java
@@ -1,5 +1,15 @@
 package model;
 
+import java.io.PrintStream;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Console {
 
+	@FXML
+	private TextArea console;
+	private PrintStream printstream;
+	
+	
 }
