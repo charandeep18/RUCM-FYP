7b253f2ce38fdda3ad6c9246799bc41f1837920f
Charandeep Rai <charandeep.rai@gmail.com>
2017-04-05 02:24:41 +0100

    Moving Code Area elements to model class

--- application/model/CodeArea.java
+++ application/model/CodeArea.java
@@ -17,6 +17,50 @@ public class CodeArea  {
 	
 private CodeArea leftCode;	
 
+private static final String[] KEYWORDS = new String[] {
+		"Given", "Then", "And", "But", "Feature", "Scenario", "When", "Background"
+};
 
-	
+private static final String KEYWORD_PATTERNS = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
+private static final String SEMICOLON = "\\;";
+private static final String STRING = "\"([^\"\\\\]|\\\\.)*\"";
+private static final String BRACKET =  "\\[|\\]";
+private static final String PARENTHESIS = "\\(|\\)";
+
+
+private static final Pattern PATTERN = Pattern.compile(
+		   "(?<KEYWORD>" + KEYWORD_PATTERNS + ")"
+		 + "|(?<SEMICOLON>" + SEMICOLON + ")"
+		 + "|(?<STRING>" + STRING + ")"
+		 + "|(?<BRACKET>" + BRACKET + ")"
+		 + "|(?<PARENTHESIS>" + PARENTHESIS + ")"
+		 );
+
+
+private static StyleSpans<Collection<String>> computeHighlighting(String text) {
+    Matcher patternmatcher = PATTERN.matcher(text);
+    StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
+    
+    int keywordend = 0;
+    
+    while(patternmatcher.find()) {
+        String styleClass = 
+        patternmatcher.group("KEYWORD") != null ? "keyword" :
+        patternmatcher.group("SEMICOLON") != null ? "semicolon" : 
+        patternmatcher.group("STRING") != null ? "string" : 
+        patternmatcher.group("BRACKET")!= null ? "bracket" : 
+        patternmatcher.group("PARENTHESIS")!= null ? "parenthesis" :
+        null;
+        assert styleClass != null;
+        spansBuilder.add(Collections.emptyList(), patternmatcher.start() - keywordend);
+        spansBuilder.add(Collections.singleton(styleClass), patternmatcher.end() - patternmatcher.start());
+        keywordend = patternmatcher.end();
+    }
+    spansBuilder.add(Collections.emptyList(), text.length() - keywordend);
+    return spansBuilder.create();
+}
+
+
+
+
 }
